6c363ac6d0f38aa178f4c374c288a7f8ce40d8e7
Romserg <serg.romankov@gmail.com>
2019-09-10 15:31:51 +0300

    Create thumbnail component

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'events-app',
-  template: `<events-list></events-list>`
+  selector: 'app-events',
+  template: `<app-events-list></app-events-list>`
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'events-list',
-  templateUrl: './events-list.component.html'
+  selector: 'app-events-list',
+  template: `
+  <div>
+    <h1>Upcoming Angular Events</h1>
+    <hr>
+    <app-event-thumbnail [event]="event"></app-event-thumbnail>
+  </div>
+  `
 })
 
 export class EventsListComponent {
