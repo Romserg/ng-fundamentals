c4b83af4e03c8f295bd42021f853db9e12cfd523
Romserg <serg.romankov@gmail.com>
2019-09-12 22:21:05 +0300

    Create template-based form and validating it

--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../user/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -25,5 +26,6 @@ import { Component } from '@angular/core';
 })
 
 export class NavbarComponent {
-
+  constructor(private auth: AuthService) {
+  }
 }
