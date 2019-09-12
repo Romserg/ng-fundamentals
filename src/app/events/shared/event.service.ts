e981c1432f5032c0bca938fb3aaaab3d92c22b95
Romserg <serg.romankov@gmail.com>
2019-09-12 17:33:43 +0300

    Pre-loading Data for components

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class EventService {
   getEvents() {
-    return EVENTS;
+    const subject = new Subject();
+    setTimeout(() => {
+      subject.next(EVENTS);
+      subject.complete();
+    }, 100);
+    return subject;
   }
 
   getEvent(id: number) {
-  return EVENTS.find(event => event.id === id);
+    return EVENTS.find(event => event.id === id);
   }
 }
 
