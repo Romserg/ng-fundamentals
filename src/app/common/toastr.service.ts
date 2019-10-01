889784ab2ace8559656df8714f77c7817a2f9995
Romserg <serg.romankov@gmail.com>
2019-10-01 16:37:26 +0300

    Remove comments

--- src/app/common/toastr.service.ts
+++ src/app/common/toastr.service.ts
@@ -9,21 +9,3 @@ export interface Toastr {
   error (msg: string, title?: string):void;
 }
 
-// @Injectable()
-// export class ToastrService {
-//   success(message: string, title?: string) {
-//     toastr.success(message, title);
-//   }
-//
-//   info(message: string, title?: string) {
-//     toastr.info(message, title);
-//   }
-//
-//   warning(message: string, title?: string) {
-//     toastr.warning(message, title);
-//   }
-//
-//   error(message: string, title?: string) {
-//     toastr.error(message, title);
-//   }
-// }
