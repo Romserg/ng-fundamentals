76b6f917e2ff661ed39eed8e900909ab28a092a9
Romserg <serg.romankov@gmail.com>
2019-10-02 17:33:49 +0300

    Using @input setter for dynamically changing of className

--- src/app/events/event-details/upvote.component.ts
+++ src/app/events/event-details/upvote.component.ts
@@ -1,14 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'upvote',
+  selector: 'app-upvote',
   styleUrls: ['./upvote.component.css'],
   template: `
     <div class="votingWidgetContainer pointable" (click)="onClick()">
       <div class="well votingWidget">
         <div class="votingButton">
-          <i *ngIf="voted" class="glyphicon glyphicon-heart"></i>
-          <i *ngIf="!voted" class="glyphicon glyphicon-heart-empty"></i>
+          <i class="glyphicon" [ngClass]="iconColor"></i>
         </div>
         <div class="badge badge-inverse votingCount">
           <div>{{count}}</div>
@@ -20,8 +19,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 export class UpvoteComponent {
   @Input() count: number;
-  @Input() voted: boolean;
+  @Input() set voted(val) {
+    this.iconColor = val ? 'glyphicon-heart' : 'glyphicon-heart-empty';
+  }
   @Output() vote = new EventEmitter();
+  iconColor: string;
 
   onClick() {
     this.vote.emit({});
